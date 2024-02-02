2022aa5a463eaa56afec01b85ccd2f32513f095f
Nico Nielsen <niconielsen.pc@gmail.com>
2024-02-02 19:12:32 -0300

    added fan rpm and battery volt

--- src/main.c
+++ src/main.c
@@ -18,8 +18,8 @@ int main() {
     
 
     sensors_init(NULL);
-    int chip_count_num = chip_count();
-
+    
+    int chip_count_num = chip_count();  
     const sensors_chip_name ** chips = get_chips();
     if (*chips == NULL) {
         fprintf(stderr, "Failed to get detected Chips");
@@ -27,8 +27,15 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    while (1) {
+    const sensors_feature * feature = NULL;
+    const sensors_feature * feature_f = NULL;
+    const sensors_feature * feature_b = NULL;
+    double feature_value_temp = 0;
+    double feature_value_rpm = 0;
+    double feature_value_volts = 0;
 
+    bool should_close = FALSE;
+    while (!should_close) {
         clear();
 
         int mid_h = max_h / 2;
@@ -36,14 +43,27 @@ int main() {
         int current_line = 0;
 
         for (int i = 0; i < chip_count_num; i++){
-            const sensors_feature * feature = get_chip_feature(chips[i], SENSORS_FEATURE_TEMP);
+            feature = get_chip_feature(chips[i], SENSORS_FEATURE_TEMP);
+            feature_f = get_chip_feature(chips[i], SENSORS_FEATURE_FAN);
+            feature_b = get_chip_feature(chips[i], SENSORS_FEATURE_IN);
             if (feature !=  NULL){
                 move(mid_h+current_line,mid_l-17);
                 printw("%10s temp :",(chips[i])->prefix);
-                double temperature;
-                sensors_get_value(chips[i], feature->number, &temperature);
-                printw(" %.2f \n",temperature);
+                sensors_get_value(chips[i], feature->number, &feature_value_temp);
+                printw(" %.2f \n",feature_value_temp);
                 current_line++;
+            }
+            if (feature_f !=  NULL) {
+               move(mid_h+current_line+3,mid_l-17);
+               printw("%11s RPM :",(chips[i])->prefix);
+               sensors_get_value(chips[i], feature_f->number, &feature_value_rpm);
+               printw(" %.2f \n", feature_value_rpm);
+            }
+            if (feature_b !=  NULL) {
+               move(mid_h+current_line+5,mid_l-17);
+               printw("%10s Volt :",(chips[i])->prefix);
+               sensors_get_value(chips[i], feature_b->number, &feature_value_volts);
+               printw(" %.2f \n", feature_value_volts);
             }
         }
 
@@ -52,15 +72,15 @@ int main() {
 
         if (getch()== 'q'){
             clear();
-            break;
+            should_close = TRUE;
         }
 
-        usleep(1000000);
+        usleep(500000);
         refresh();
     }
 
     free(chips);
     sensors_cleanup();
-    endwin();
+    endwin(); // and this called enwin????? 
     return EXIT_SUCCESS;
 }
